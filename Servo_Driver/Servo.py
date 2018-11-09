75f3f65f30158f7ede02b141da907922f569e5d9
Andreas Rekdal <andrrekd@stud.ntnu.no>
2018-11-09 12:21:51 +0100

    initialize threads in list

--- Servo_Driver/Servo.py
+++ Servo_Driver/Servo.py
@@ -9,17 +9,18 @@ from adafruit_motor import servo
 
 
 class Servo(Thread):
-    def __init__(self):
+    def __init__(self, port):
+        self.port = port
         Thread.__init__(self)
         i2c = busio.I2C(SCL, SDA)
         pca = PCA9685(i2c)
         pca.frequency = 50
-        servo1 = servo.Servo(pca.channels[1])
+        self.servo1 = servo.Servo(pca.channels[1])
 
     def run(self):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
-            s.bind(("127.0.0.1", 8001))
+            s.bind(("127.0.0.1", self.port))
         except socket.error as msg:
             print("bind failed", msg)
             s.listen(10)
@@ -28,14 +29,16 @@ class Servo(Thread):
             connection, address = s.accept()
             print("Connected with" , address[0], ":", str(address[1]))
             print(connection.recv(1024))
-            servo1.angle = connection.recv(1024)
+            self.servo1.angle = connection.recv(1024)
 
 
 if __name__ == '__main__':
-    servo1 = Servo()
-    servo1.setName("Servo 1")
-    servo2 = Servo()
-    servo2.setName("Servo 2")
+    servos = []
+    for i in range(6):
+        port = 8011 + i
+        servos[i] = Servo(port)
+        servos[i].setName("Servo " + i)
 
-    servo1.start()
-    servo2.start()
+    for i in len(servos):
+        servos[i].start()
+
