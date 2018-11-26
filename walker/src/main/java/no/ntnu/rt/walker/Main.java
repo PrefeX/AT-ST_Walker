9777cce5902bae4e954452780d4c66d71eef619d
Andreas Rekdal <andrrekd@stud.ntnu.no>
2018-11-26 21:02:16 +0100

    added sleep to allow pyhon server to start

--- walker/src/main/java/no/ntnu/rt/walker/Main.java
+++ walker/src/main/java/no/ntnu/rt/walker/Main.java
@@ -15,7 +15,7 @@ public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         Process execPython = Runtime.getRuntime()
                 .exec("python3 /home/pi/AT-ST_Walker/Servo_Driver/Servo.py");
-        
+        Thread.sleep(2000);
         Walker walker = new Walker();
         walker.walk();    
         
